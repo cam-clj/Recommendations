3a6bcbccf91376caf216ad8508be24302db3201e
Ray Miller <ray@1729.org.uk>
2013-08-17 21:02:42 +0100

    Add data.priority-map to dependencies

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.csv "0.1.2"]
+                 [org.clojure/data.priority-map "0.0.2"]
                  [incanter "1.5.2"]])
